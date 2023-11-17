5eb54d42ac2a971220ef7886663da505f30d9ed7
Rayen Akrich <akricherayen@gmail.com>
2023-11-17 17:57:19 +0100

    Create Project_3.c

---
+++ Project_3.c
@@ -0,0 +1,27 @@
+//le but de ce programme est de saisir Un tableau de 5<=N<=20 chaines de caractere ,
+//puis extraiter les lettres de chaque case et les afficher.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include <ctype.h>
+int main() {
+    char T[20][10];
+    char ST[10];     //ST sera le nom du variable contenant les lettres dans une Chaine T[i]
+    int N;
+    do {
+        printf("Saisir N (entre 5 et 20) : ");
+        scanf("%d", &N);
+    } while (N < 5 || N > 20);
+    for (int i = 0; i < N; i++) {
+        ST[0] = '\0';
+        printf("Saisir la %d ème case : ", i);
+        scanf("%s", T[i]);fflush(stdout);
+        for (int j = 0; T[i][j] != '\0'; j++) {
+            if (isalpha(T[i][j])) {
+                strncat(ST, &T[i][j], 1);
+            }
+        }
+        printf("%d %s \n",i,ST);
+    }
+}
