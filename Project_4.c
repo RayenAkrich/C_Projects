8f0998b0a60d43f49aa80b2699e6e462fa3305b7
Rayen Akrich <akricherayen@gmail.com>
2023-11-17 19:18:02 +0100

    Create Project_4.c

---
+++ Project_4.c
@@ -0,0 +1,27 @@
+//Le but de ce programme est de saisir une chaine de taille N est de remplacer toute suite d’un même caractère par un seul.
+
+#include <stdio.h>
+#include <stdlib.h>
+void main(){
+    char *T;
+    int N;
+    printf("Donner la taille de votre chaine: ");
+    scanf("%d", &N);
+    T=(char *)malloc((N + 1) * sizeof(char));
+    if(T==NULL){printf("erreur");}
+    else{free(T);}
+    printf("Donner votre chaine: \n");
+    fflush(stdin);
+    gets(T);
+    for (int i=0;i<=N-2;i++){
+        if (T[i]==T[i+1]){
+            for (int j=i;j<=N-1;j++){
+                T[j]=T[j+1];
+            }
+            N=N-1;
+            i=i-1;
+        }
+    }
+    T[N]='\0';
+    printf("votre chaine sans répétition est :\n %s",T);
+}
