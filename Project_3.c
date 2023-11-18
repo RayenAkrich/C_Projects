0bed60b69eb4b5b522d339e256e83bbf11a10752
Rayen Akrich <akricherayen@gmail.com>
2023-11-18 16:46:08 +0100

    Update Project_3.c

--- Project_3.c
+++ Project_3.c
@@ -1,7 +1,6 @@
 //Le but de ce programme est de saisir Un tableau de 5<=N<=20 chaines de caractere ,puis extraiter les lettres de chaque case et les afficher.
 
 #include<stdio.h>
-#include<stdlib.h>
 #include <string.h>
 #include <ctype.h>
 int main() {
