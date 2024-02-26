ffd3cb63d3e8588425c9f57a3fcbf4ae19ea96bd
Rayen Akrich <akricherayen@gmail.com>
2024-02-26 19:35:32 +0100

    Update Project_33.c

--- Project_33.c
+++ Project_33.c
@@ -1,6 +1,5 @@
-//Le but de ce programme est de remplir une liste doublement chainée en date et température ,
-//supprime de cases selon le choix de la temperature par l'utilisateur
-//ou bien ajouter une case après une temperature donné par l'utilisateur
+//Le but de ce programme est de remplir une liste doublement chainée en date et température ,supprime de cases selon le choix de la temperature par l'utilisateur
+//ou bien ajouter une case apres une temperature donné par l'utilisateur
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -163,52 +162,42 @@ int main()
 {
     T *p;
     T *p2;
+    int test_ajout;
+    float temperture_X;
 
 
     while(1){
         // Choix de l'utilisateur
         int choix;
         do{
-            printf("\n0: Affichage de la liste\n1: Ajouter une case\n2: Supprimer les cases selon une temperature\n3: Ajouter une case apres une temperature\n4: Sortir du programme\n_____");
+            printf("\n0: Affichage de la liste\n1: Ajouter une case\n2: Supprimer les cases selon une temperature\n3: Ajouter une case apres une temperature\n4: Sortir du programme\n_____\n");
             scanf("%d", &choix);} while (!(choix==1 || choix==2 || choix==3 || choix==4 || choix==0));
-
-        // Choix 0
-        if(choix==0){
-            Afficher_Liste(p);
-        }
-
-        // Choix 1
-        if(choix==1){
-            int test_ajout;
-            do{
-                Add_Element(&p);
-                // Demande à l'utilisateur s'il veut ajouter une autre case
-                printf("Voulez vous ajouter une autre case ? (1 pour oui, 0 pour non): ");
-                scanf("%d", &test_ajout);
-            }while(test_ajout==1);
-        }
-
-        // Choix 2
-        if(choix==2){
-            //Saisie de la temprerature qu'on veut supprimer
-            float temperture_X;
-            printf("\nQuelle est la temperature que vous voulez supprimer:\n");
-            scanf("%f",&temperture_X);
-            Supprimer_case(p,temperture_X);
-        }
-
-        // Choix 3
-        if(choix==3){
-            //Saisie de la temprerature qu'on veut ajouter apres lui
-            float temperture_X;
-            printf("\nQuelle est la temperature que vous voulez ajouter apres lui:\n");
-            scanf("%f",&temperture_X);
-            Add_Element_After_X(&p,temperture_X);
-        }
-
-        // Choix 4
-        if(choix==4){
-            Sortir(p);
+        
+        switch (choix) {
+            case 0:
+                Afficher_Liste(p);break;
+            case 1:
+                do{
+                    Add_Element(&p);
+                    // Demande à l'utilisateur s'il veut ajouter une autre case
+                    printf("Voulez vous ajouter une autre case ? (1 pour oui, 0 pour non): ");
+                    scanf("%d", &test_ajout);
+                }while(test_ajout==1);
+                break;
+            case 2:
+                //Saisie de la temprerature qu'on veut supprimer
+                printf("\nQuelle est la temperature que vous voulez supprimer:\n");
+                scanf("%f",&temperture_X);
+                Supprimer_case(p,temperture_X);break;
+            case 3:
+                //Saisie de la temprerature qu'on veut ajouter apres lui
+                printf("\nQuelle est la temperature que vous voulez ajouter apres lui:\n");
+                scanf("%f",&temperture_X);
+                Add_Element_After_X(&p,temperture_X);break;
+            case 4:
+                Sortir(p);break;
+            default:
+                break;
         }
     }
 }
