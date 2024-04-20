1e9fee853f9925ada68f8963cf34aba7d9c7096b
Rayen Akrich <akricherayen@gmail.com>
2024-04-20 16:15:38 +0100

    Create fonctions.h

---
+++ Arbre binaire/fonctions.h
@@ -0,0 +1,14 @@
+#ifndef UNTITLED_FONCTIONS_H
+#define UNTITLED_FONCTIONS_H
+
+//Type arbre binaire
+struct Arbre_b{
+    struct Arbre_b **F_d; //Fils droit
+    struct Arbre_b **F_g; //Fils gauche
+    int a; //Type de l'information est un entier 
+};
+typedef struct Arbre_b Arbre;
+
+Arbre* create(int n); //n est le nombre de niveaux
+
+#endif //UNTITLED_FONCTIONS_H
