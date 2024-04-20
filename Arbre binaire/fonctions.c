a64d229d062a2bb02b71816ede6e99e32e3617d5
Rayen Akrich <akricherayen@gmail.com>
2024-04-20 16:25:21 +0100

    Create fonctions.c

---
+++ Arbre binaire/fonctions.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fonctions.h"
+
+Arbre* create(int n) {
+    //Cas de base si n est 0
+    if (n == 0) {
+        return NULL;
+    }
+
+    //Initialisation d'une nouvelle arbre
+    Arbre* nouveau = (Arbre*)malloc(sizeof(Arbre));
+    if(nouveau==NULL){
+      printf("Erreur d'allocation de mémoire!");
+      return NULL;
+    }
+    
+    //Initialisation les fils à NULL
+    nouveau->F_d = NULL;
+    nouveau->F_g = NULL;
+
+    //Saisie de la valeur de sommet du père
+    printf("Saisir la valeur du sommet du père: ");
+    scanf("%d",&nouveau->a);
+    
+    // Crée les sous-arbres récursivement
+    nouveau->F_g = create(n - 1);
+    nouveau->F_d = create(n - 1);
+    
+    return nouveau;
+}
